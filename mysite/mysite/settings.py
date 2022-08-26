efe93bd00fca71b4d827820cf580fb7f8d7dab36
LAYS KASSIANE ARAÚJO DOS SANTOS <2020314881@IFAL.LOCAL>
2022-08-26 10:07:49 -0300

    parte 3 e metade da parte 4

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'laysgostosa',
+        'NAME': 'laysboa',
         'USER': 'root',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
